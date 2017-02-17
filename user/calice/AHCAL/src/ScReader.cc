0c795b3a04bbf0ba6dda2ca6b3bf5386b104af05
Jiri Kvasnicka <jiri.kvasnicka@desy.de>
2017-02-17 12:01:45 +0100

    add ROC tag

--- user/calice/AHCAL/src/ScReader.cc
+++ user/calice/AHCAL/src/ScReader.cc
@@ -576,7 +576,7 @@ namespace eudaq {
                eudaq::EventUP nev = eudaq::RawDataEvent::MakeUnique("CaliceObject");
                eudaq::RawDataEvent *nev_raw = dynamic_cast<RawDataEvent*>(nev.get());
                prepareEudaqRawPacket(nev_raw);
-
+               nev->SetTag("ROC",roc);
                nev->SetTriggerN(rawTrigId - _producer->getLdaTrigidOffset());
                if (startTS && (!_producer->getIgnoreLdaTimestamps())) {
                   nev->SetTimestampBegin(startTS + _producer->getAhcalbxid0Offset() + bxid * _producer->getAhcalbxidWidth() - 1);
@@ -665,6 +665,7 @@ namespace eudaq {
             eudaq::EventUP nev = eudaq::RawDataEvent::MakeUnique("CaliceObject");
             eudaq::RawDataEvent *nev_raw = dynamic_cast<RawDataEvent*>(nev.get());
             prepareEudaqRawPacket(nev_raw);
+            nev->SetTag("ROC",roc);
 
             if (startTS && (!_producer->getIgnoreLdaTimestamps())) {
                nev->SetTimestampBegin(startTS + _producer->getAhcalbxid0Offset() + bxid * _producer->getAhcalbxidWidth() - 1);
@@ -715,6 +716,7 @@ namespace eudaq {
                std::cout << "ERROR EB: Length of the acquisition is longer than 100 ms in ROC " << roc << std::endl;
                if (_producer->getColoredTerminalMessages()) std::cout << "\033[0m";
             }
+            nev->SetTag("ROC",roc);
             std::vector<uint32_t> cycledata;
             cycledata.push_back((uint32_t) (_LDATimestampData[roc].TS_Start));
             cycledata.push_back((uint32_t) (_LDATimestampData[roc].TS_Start >> 32));
@@ -794,7 +796,7 @@ namespace eudaq {
                      }
                      break;
                }
-
+               nev->SetTag("ROC",roc);
                //copy the ahcal data
                if (i == (_LDATimestampData[roc].TS_Triggers.size() - 1)) {
                   //the last triggerID in the vector
